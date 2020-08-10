fcbf567b7f87f6b234e24729f4988aa1f9d76721
Nath-G <nathalie.greselin@gmail.com>
2020-08-10 21:12:57 +0200

    mysql works and clean

--- srcs/containers/mysql/srcs/start.sh
+++ srcs/containers/mysql/srcs/start.sh
@@ -1,39 +1,3 @@
-# #!/bin/sh
-# apk upgrade
-# apk add openrc
-# apk add mysql mysql-client --no-cache
-# # apk add telegraf --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted --no-cache
-
-# mkdir -p /run/mysqld
-# cp -rp /var/run/mysqld /var/run/mysqld.bak
-
-# openrc
-# touch /run/openrc/softlevel
-
-# mysql_install_db --user=root
-# tmp=sql_tmp
-# # GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;\n
-
-# echo -ne "FLUSH PRIVILEGES;\n
-# GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;\n
-# FLUSH PRIVILEGES;\n" >> $tmp 
-
-# # CREATE USER 'username'@'localhost' IDENTIFIED BY 'password';
-
-# # GRANT ALL PRIVILEGES ON *.* TO 'username'@'localhost' WITH GRANT OPTION;
-
-# # CREATE USER 'username'@'%' IDENTIFIED BY 'password';
-
-# # GRANT ALL PRIVILEGES ON *.* TO 'username'@'%' WITH GRANT OPTION;
-
-# # FLUSH PRIVILEGES;
-
-
-# /usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tmp
-# rm -rf $tmp
-
-# exec /usr/bin/mysqld --user=root
-
 #!/bin/sh
 
 mysql_install_db --user=root --ldata=/var/lib/mysql
@@ -47,17 +11,7 @@ echo "GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION;" >> $tmp
 echo "GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;" >> $tmp
 echo "FLUSH PRIVILEGES;" >> $tmp
 
-# echo -ne "FLUSH PRIVILEGES;\n
-# GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;\n
-# GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;\n
-# FLUSH PRIVILEGES;\n" >> $tmp 
-# echo -ne "FLUSH PRIVILEGES;\n
-# GRANT ALL ON *.* TO '__DB_USER__'@'%' identified by '__DB_PASSWORD__' WITH GRANT OPTION ;\n
-# GRANT ALL PRIVILEGES ON *.* TO '__DB_USER__'@'localhost' IDENTIFIED BY '__DB_PASSWORD__' WITH GRANT OPTION;\n
-# FLUSH PRIVILEGES;\n" >> $tmp
-# /usr/bin/mysqld --user=__DB_USER__ --bootstrap --verbose=0 < $tmp
 /usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tmp
-# /usr/bin/mysqld_safe --datadir='./data'
 
 # rm -rf $tmp
 # exec /usr/bin/mysqld --user=__DB_USER__
