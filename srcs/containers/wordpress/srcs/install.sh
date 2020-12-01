f6a6f16ff419ab449f74285035f491889935fe8e
Nath-G <nathalie.greselin@gmail.com>
2020-12-01 20:00:18 +0100

    telegraf works

--- srcs/containers/wordpress/srcs/install.sh
+++ srcs/containers/wordpress/srcs/install.sh
@@ -1,9 +1,15 @@
-apk update
+# apk update
+# apk add openrc
+# apk add telegraf --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted --no-cache
+
+# openrc
+# touch /run/openrc/softlevelrc-update
+
+# rc-update add telegraf
+apk upgrade
 apk add openrc
 apk add telegraf --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted --no-cache
 
 openrc
-touch /run/openrc/softlevelrc-update
-# rc update add php-fpm7 default #ajout
-
+touch /run/openrc/softlevel
 rc-update add telegraf
--- srcs/containers/wordpress/srcs/setup.sh
+++ srcs/containers/wordpress/srcs/setup.sh
@@ -1,4 +1,5 @@
 # tail -f /var/log/nginx/error.log -n 0 &
 # service php-fpm7 stop
+#!/bin/bash
 php-fpm7
 nginx -g 'daemon off;'
